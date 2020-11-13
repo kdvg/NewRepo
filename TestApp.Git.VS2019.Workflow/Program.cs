4e40c390c4b3d600f8c9f3508016727e7cfb131b
kpp 567 <kpp567567@gmail.com>
2020-11-12 18:48:58 -0800

    First commit

---
+++ TestApp.Git.VS2019.Workflow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApp.Git.VS2019.Workflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
